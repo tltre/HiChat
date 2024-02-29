7276d3d91dfd6cc2f9ec7822b34067514323962b
熹 翁 <1225385267@qq.com>
2024-03-01 00:12:37 +0800

    add the model of user_basic.go

---
+++ src/models/user_basic.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"gorm.io/gorm"
+
+	"time"
+)
+
+type Model struct {
+	ID        uint `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
