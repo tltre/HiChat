63f9b64e1339c0b3d62edd1061f0cdfd93731099
熹 翁 <1225385267@qq.com>
2024-03-01 00:34:50 +0800

    modifier user_basic.go

--- src/models/user_basic.go
+++ src/models/user_basic.go
@@ -12,3 +12,22 @@ type Model struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
+
+type UserBasic struct {
+	Model
+	Name          string
+	PassWord      string
+	Avatar        string
+	Gender        string `gorm:"column:gender;default:male;type:varchar(6) comment 'male表示男， famale表示女'"` //gorm为数据库字段约束
+	Phone         string `valid:"matches(^1[3-9]{1}\\d{9}$)"`                                             //valid为条件约束
+	Email         string `valid:"email"`
+	Identity      string
+	ClientIp      string `valid:"ipv4"`
+	ClientPort    string
+	Salt          string     //盐值
+	LoginTime     *time.Time `gorm:"column:login_time"`
+	HeartBeatTime *time.Time `gorm:"column:heart_beat_time"`
+	LoginOutTime  *time.Time `gorm:"column:login_out_time"`
+	IsLoginOut    bool
+	DeviceInfo    string //登录设备
+}
