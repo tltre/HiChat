a0d8838e301bd3debdbe45572ab1dc2c6d2d3017
熹 翁 <1225385267@qq.com>
2024-03-01 16:56:04 +0800

    modifier the struct of /test/main.go

--- src/test/main.go
+++ src/test/main.go
@@ -1,25 +1,11 @@
 package main
 
-import (
-	"HiChat/src/config"
-	"HiChat/src/models"
-	"fmt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
 func main() {
-	// declare the connection to the DB
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		config.User, config.Password, config.Host, config.Port, config.DBName)
-	// connect to the Database
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+	// connect to Database
+	db := ConnectToDatabase()
+
 	// create the user_basic table
-	err = db.AutoMigrate(&models.UserBasic{})
-	if err != nil {
-		panic(err)
-	}
+	CreateUserTable(db)
+
+	// test User table
 }
